110298185ecaa8b0cd1d4a926dda1ec4fcc389dd
“Brandon <brandon.j.bryant1@gmail.com>
2020-12-23 12:18:26 -0600

    Tables Exercise 2

--- tables_exercises.sql
+++ tables_exercises.sql
@@ -1,22 +1,21 @@
-
-#Use employees database/ List all tables in Database
-SHOW Databases;
-
+#Open Sequel Pro and login to the database server
+#Save your work in a file named tables_exercises.sql
+#Use the employees database
+#List all the tables in the database
+	SHOW TABLES;
 #Explore the employees table. What different data types are present on this table?
-SHOW TABLES;
-
+#int, date, varchar, enum
+	DESCRIBE employees;
 #Which table(s) do you think contain a numeric type column?
-Salaries, dept_emp_latest_date, titles, employees
-
+	#dept_emp_lastest_date, employees, employees_with_departments, salaries
+	
 #Which table(s) do you think contain a string type column?
-`current_dept_emp` `departments` `dept_emp` `dept_manager` titles
-
+	#all
 #Which table(s) do you think contain a date type column?
-`dept_emp_latest_date` 
-
+	#dept_emp_latest_date, current_dept_emp
+	
 #What is the relationship between the employees and the departments tables?
-DESCrIBE `departments`;
-employees are assigned to departments.
+	#Employes are categorized by department.
 
 #Show the SQL that created the dept_manager table.
-SHOW CREATE TABLE `dept_manager`
+	SHOW CREATE TABLE dept_manager;
