ebdafb6210d2e20de241c4f586cc8ca3427b58e6
“Brandon <brandon.j.bryant1@gmail.com>
2021-01-07 15:31:14 -0600

    First take

---
+++ case_exercises.sql
@@ -0,0 +1,11 @@
+Create a file named case_exercises.sql and craft queries to return the results for the following criteria:
+
+Write a query that returns all employees (emp_no), their department number, their start date, their end date, and a new column 'is_current_employee' that is a 1 if the employee is still with the company and 0 if not.
+
+Write a query that returns all employee names (previous and current), and a new column 'alpha_group' that returns 'A-H', 'I-Q', or 'R-Z' depending on the first letter of their last name.
+
+How many employees (current or previous) were born in each decade?
+
+BONUS
+
+What is the current average salary for each of the following department groups: R&D, Sales & Marketing, Prod & QM, Finance & HR, Customer Service?
