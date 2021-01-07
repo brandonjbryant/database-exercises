16272b7e731463dd2f6d6b183a52d0ccb617cede
“Brandon <brandon.j.bryant1@gmail.com>
2021-01-07 10:21:29 -0600

    Temporary Table Exercises Progression

--- temporary_tables.sql
+++ temporary_tables.sql
@@ -1,65 +1,124 @@
-# When Doing Temp Tables
-# 3 Steps to Success w/ Temp Tables
-# Step 1: Use your own database as a scratchpad
-# Step 2: Write the query you need to, using the database_name.table_name after your FROM or with your JOINS
-# Step 3: Wrap your query from above in CREATE TEMPORARY TABLE table_name AS ();
+#1. Using the example from the lesson, re-create the employees_with_departments table.
+USE easley_1267;
 
-# Start by using your own database (so you can write/edit/delete/whatever)
+CREATE TEMPORARY TABLE employees_with_departments_copy (select *
+from employees.employees_with_departments);
 
-# Step 1- use your own DB
-# use easley_****;
-use ryan;
+SELECT * 
+from employees_with_departments_copy;#verify
 
-# Step 2 - write whatever query you need, being 100% sure to use database_name.table_name syntax
+#a. Add a column named full_name to this table. It should be a VARCHAR whose length is the sum of the lengths of the first name and last name columns
+ALTER TABLE ADD full_name VARCHAR(30);
+
+#b. Update the table so that full name column contains the correct data
+UPDATE employees_with_departments_copy
+SET full_name = CONCAT(first_name,' ',last_name); 
+
+#c. Remove the first_name and last_name columns from the table.
+ALTER TABLE employees_with_departments_copy DROP COLUMN first_name;
+ALTER TABLE employees_with_departments_copy DROP COLUMN last_name;
+
+
+
+#d. What is another way you could have ended up with this same table?
+DROP TEMPORARY TABLE employees_with_departments_copy;
+
+CREATE TEMPORARY TABLE employees_with_departments_copy (SELECT emp_no, dept_no, dept_name, 	CONCAT(first_name,' ',last_name) AS full_name
+FROM employees.employees_with_departments);
+
+SELECT *
+FROM employees_with_departments_copy;
+
+#2. Create a temporary table based on the payment table from the sakila database.Write the SQL necessary to transform the amount column such that it is stored as an integer representing the number of cents of the payment. For example, 1.99 should become 199.
+CREATE TEMPORARY TABLE sakila_payments (SELECT * 
+	from sakila.payment);
+
+SELECT *
+FROM sakila_payments;
+
+ALTER TABLE sakila_payments ADD new_amount INT;
+
+
+UPDATE sakila_payments 
+SET new_amount = amount * 100;
+
+SELECT * 
+FROM sakila_payments;
+
+
+
+
+
+#3. Find out how the current average pay in each department compares to the overall, historical average pay. In order to make the comparison easier, you should use the Z-score for salaries. In terms of salary, what is the best department right now to work for? The worst?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#temp table for single value used later in z-score calc
+create temporary table historical_avg_salary (select avg(salary) 
+from employees.salaries); #$63810.7448
+
+#sanity check
 select *
-from employees.employees # database_name.table_name
-join employees.salaries using(emp_no) # database_name.table_name
-where to_date > curdate()
+from historical_avg_salary;
+
+#temp table with dept_name and avg_salary grouped by dept_name
+create temporary table department_avg_salary (select dept_name, avg(salary) as avg_salary
+from employees.salaries
+join employees.dept_emp using (emp_no)
+join employees.departments using (dept_no)
+where employees.salaries.to_date > curdate()
+group by dept_name
+ORDER BY avg_salary Desc);
+
+#sanity check
+select * 
+from department_avg_salary;
 
 
-# Step 3 - wrap the query in a CREATE TEMPORARY TABLE table_name AS ();
-# Let's make a temp table of current employees w/ their salary
-create temporary table emp_salary as (
-	select *
-	from employees.employees # database_name.table_name
-	join employees.salaries using(emp_no) # database_name.table_name
-	where to_date > curdate()
-);
+#added z-score column using z=(x-u)/stddev
+select *, round(
+				(avg_salary - (select * from historical_avg_salary))
+			                         /
+				(select stddev(salary)from employees.salaries)
+				
+				,3) as z_score
+				
+from department_avg_salary;
+					
+/*					Current
+Dept_name			Avg_salary  Z-score
+Sales				88842.1590	 1.481
+Marketing			80014.6861	 0.959
+Finance				78644.9069	 0.878
+Research			67932.7147	 0.244
+Production			67841.9487	 0.238
+Development			67665.6241	 0.228
+Customer Service	66971.3536	 0.187
+Quality Management	65382.0637	 0.093
+Human Resources		63795.0217	-0.001
+*/
 
-select *
-from emp_salary;
-
-# After we have a temp table, we can get get to work:
-# Querying from it
-# Altering data (if we need to)
-
-# Stakeholder says: 
-# What would our total salary spend be if everybody gets a 5% raise?
-# Technically we don't need a temp table to solve this, so this is a simple example :)
-update emp_salary
-set salary = salary + salary * .05;
-
-select * from emp_salary;
-
-select sum(salary) from emp_salary;
-
-
-select *
-from emp_salary
-where first_name = "Mary"
-and salary > 60000;
-
-#Create a file named temporary_tables.sql to do your work for this exercise.
-
-#Using the example from the lesson, re-create the employees_with_departments table.
-
-#Add a column named full_name to this table. It should be a VARCHAR whose length is the sum of the lengths of the first name and last name columns
-#Update the table so that full name column contains the correct data
-#Remove the first_name and last_name columns from the table.
-#What is another way you could have ended up with this same table?
-#Create a temporary table based on the payment table from the sakila database.
-
-#Write the SQL necessary to transform the amount column such that it is stored as an integer representing the number of cents of the payment. For example, 1.99 should become 199.
-
-#Find out how the current average pay in each department compares to the overall, historical average pay. In order to make the comparison easier, you should use the Z-score for salaries. In terms of salary, what is the best department right now to work for? The worst?;
-
+#Sales department is best currently with an average salary 1.48 standard deviations above the historical mean salary for the entire company and HR is the worst which is slightly below the historical mean salary.
