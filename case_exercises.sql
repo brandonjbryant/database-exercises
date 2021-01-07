d05f57362689b9cdc65afb7c78c0805d4b7a6b92
“Brandon <brandon.j.bryant1@gmail.com>
2021-01-07 17:00:30 -0600

    First Excercise Complete

--- case_exercises.sql
+++ case_exercises.sql
@@ -1,11 +1,20 @@
-Create a file named case_exercises.sql and craft queries to return the results for the following criteria:
 
-Write a query that returns all employees (emp_no), their department number, their start date, their end date, and a new column 'is_current_employee' that is a 1 if the employee is still with the company and 0 if not.
 
-Write a query that returns all employee names (previous and current), and a new column 'alpha_group' that returns 'A-H', 'I-Q', or 'R-Z' depending on the first letter of their last name.
+-- Write a query that returns all employees (emp_no), their department number, their start date, their end date, and a new column 'is_current_employee' that is a 1 if the employee is still with the company and 0 if not.
 
-How many employees (current or previous) were born in each decade?
+   	
+   		use employees;
+   		SELECT emp_no, dept_no, hire_date, to_date, 
+   			IF(to_date > curdate(), TRUE ,FALSE) AS is_current_employee 
+   		FROM employees
+   			JOIN dept_emp USING(emp_no);
+   		
+   		
+   		
+-- Write a query that returns all employee names (previous and current), and a new column 'alpha_group' that returns 'A-H', 'I-Q', or 'R-Z' depending on the first letter of their last name.
 
-BONUS
+-- How many employees (current or previous) were born in each decade?
 
-What is the current average salary for each of the following department groups: R&D, Sales & Marketing, Prod & QM, Finance & HR, Customer Service?
+-- BONUS
+
+-- What is the current average salary for each of the following department groups: R&D, Sales & Marketing, Prod & QM, Finance & HR, Customer Service?
